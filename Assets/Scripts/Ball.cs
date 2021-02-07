7d672aec776a5e9a4a9371f7b4d9d37d865fd8d9
Niko <70172259+NikoCSUMB@users.noreply.github.com>
2021-02-07 12:14:48 -0800

    FINAL FINAL
    ALL IS DONE

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,10 +7,12 @@ public class Ball : MonoBehaviour
     public float speed = 1;
     public Vector3 previousVelocity; // Should probably use a get function instead of making this public but w/e 
     Rigidbody rb;
+    TrailRenderer trail;
 
     // Start is called before the first frame update
     void Start()
     {
+        trail = this.GetComponent<TrailRenderer>();
         rb = this.GetComponent<Rigidbody>();
         rb.velocity = new Vector3(-4, 2, 0);
         
@@ -20,6 +22,7 @@ public class Ball : MonoBehaviour
 
     public void Restart(int direction)
     {
+        trail.time = 0;
         speed = 4;
         rb.position = new Vector3(0, 1.29f, -3);
         rb.velocity = new Vector3(4 * direction, 2, 0);
@@ -28,6 +31,9 @@ public class Ball : MonoBehaviour
 
 
     void FixedUpdate(){
+        if (trail.time < 0.5f){
+            trail.time += 0.006f;
+        }
         previousVelocity = rb.velocity;
     }
 }
